21aaf9f5523d3643f72322c452c22222c06c68d7
Clémence LHERMITE <clemencelhermite@MacBook-Air-de-Christopher.home>
2021-06-25 12:16:32 +0200

    Mise en place de l'api

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 fetch("http://localhost:3000/api/furniture")
-.then(response => console.log(response))
+.then(response => response.json())
+.then(response => alert(JSON.stringify(response)))
+.catch(error => alert("Erreur : " + error));
