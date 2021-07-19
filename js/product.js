d2ec19be8134c7d4362ba606a4d5a82b0e30a890
Clémence LHERMITE <clemencelhermite@MacBook-Air-de-Christopher.home>
2021-07-19 16:08:58 +0200

    Page produits

--- js/product.js
+++ js/product.js
@@ -1,30 +1,28 @@
-//Get id from url
+// Get id from url
 const queryString = window.location.search
 const urlParams = new URLSearchParams(queryString)
 
 const productId = urlParams.get('id')
 
-
-
 window
-.fetch('http://localhost:3000/api/furniture/' + productId)
-.then(response => response.json())
-.then(response => {
-    if (Object.entries(response). length === 0) {
-        window.alert('pas de produit')
+  .fetch('http://localhost:3000/api/furniture/' + productId)
+  .then(response => response.json())
+  .then(response => {
+    if (Object.entries(response).length === 0) {
+      window.alert('pas de produit')
     } else {
-        createProduct(response)
+      createProduct(response)
     }
-})
-.catch(error => window.alert('Erreur : ' + error))
+  })
+  .catch(error => window.alert('Erreur : ' + error))
 
 const createProduct = product => {
-    //Edit product name
-    const productName = document.getElementById('product-name')
-    productName.innerHTML = product.name
+  // Edit product name
+  const productName = document.getElementById('product-name')
+  productName.innerHTML = product.name
 
-    //Edit product Image
-    const productImage = document.getElementById('product-image')
-    productImage.src = product.imageUrl
-    productImage.alt = product.name
+  // Edit product image
+  const productImage = document.getElementById('product-image')
+  productImage.src = product.imageUrl
+  productImage.alt = product.name
 }
