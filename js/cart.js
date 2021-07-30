b3b67c94c9b425b817a91d47bd8fd9ff9cde491a
Clémence LHERMITE <clemencelhermite@MacBook-Air-de-Christopher.home>
2021-07-30 14:23:39 +0200

    Page panier

--- js/cart.js
+++ js/cart.js
@@ -1,2 +1,23 @@
+//Récupération du formulaire
+let form = document.getElementById("myform");
 
+
+form.onsubmit = function() {
+    let username = document.getElementById("username")
+    let usermail = document.getElementById("usermail")
+    let phone = document.getElementById("phone")
+    let adress = document.getElementById("adress")
+    let code = document.getElementById("code")
+    let city = document.getElementById("city")
+
+    if (username.value && usermail.value && phone.value && adress.value && code.value && city.value) {
+        
+       
+//Si les champs ne sont pas renseignés
+    }else {
+        alert("Veuillez saisir tous les champs");
+    }
+
+    
+}
 
